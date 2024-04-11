6f2d62f47a205cdf3b01c3b4d8406ce541be7e04
hanihoneykim <107341819+hanihoneykim@users.noreply.github.com>
2024-04-11 16:58:30 +0900

    add: myeongdong_register.html

--- the_myeongdong/urls.py
+++ the_myeongdong/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from the_myeongdong import views
 
 urlpatterns = [
-    path("main", views.Main, name="main"),
+    path("reservations", views.reservation, name="reservations"),
+    path("reservations/register", views.register, name="register"),
 ]
--- the_myeongdong/views.py
+++ the_myeongdong/views.py
@@ -7,3 +7,13 @@ from django.core.paginator import Paginator
 def Main(request):
     context = {}
     return render(request, "pages/index.html", context)
+
+
+def reservation(request):
+    context = {}
+    return render(request, "pages/get_reservation.html", context)
+
+
+def register(request):
+    context = {}
+    return render(request, "pages/myeongdong_register.html", context)
