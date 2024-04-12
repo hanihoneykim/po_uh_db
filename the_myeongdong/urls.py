fc13f8849f3a56dec308020dcaa07b253ba1959e
hanihoneykim <107341819+hanihoneykim@users.noreply.github.com>
2024-04-12 12:00:56 +0900

    edit: myeongdong_reservations.html with dummy data

--- the_myeongdong/urls.py
+++ the_myeongdong/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from the_myeongdong import views
 
 urlpatterns = [
-    path("reservations", views.reservation, name="reservations"),
+    # path("reservations", views.reservation, name="reservations"),
     path("reservations/register", views.register, name="register"),
+    path("reservations", views.ReservationListView.as_view(), name="reservations"),
 ]
--- the_myeongdong/views.py
+++ the_myeongdong/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from datetime import date, datetime
 from rest_framework import generics, status
 from django.core.paginator import Paginator
+from .models import MyeongdongReservation
 
 
 def Main(request):
@@ -17,3 +18,11 @@ def reservation(request):
 def register(request):
     context = {}
     return render(request, "pages/myeongdong_register.html", context)
+
+
+class ReservationListView(generics.ListCreateAPIView):
+    template_name = "pages/myeongdong_reservation.html"
+
+    def get(self, request):
+        # reservations = MyeongdongReservation.objects.all()
+        return render(request, "pages/myeongdong_reservation.html")
