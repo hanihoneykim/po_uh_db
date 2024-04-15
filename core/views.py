649a9310280ad9219194e592e21e031c28c94b08
hanihoneykim <107341819+hanihoneykim@users.noreply.github.com>
2024-04-15 14:17:07 +0900

    edit: GuestListView parameter search

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.response import Response
+from django.http import HttpResponseRedirect
 from django.db.models import Q
 from datetime import date, datetime
 from rest_framework import generics, status
@@ -16,21 +17,24 @@ def branch(request):
 class GuestListView(generics.ListCreateAPIView):
     template_name = "pages/guest/guest_inquiry.html"
     serializer_class = GuestSerializer
-    queryset = Guest.objects.all()
 
-    def get(self, request, *args, **kwargs):
-        queryset = self.queryset
+    def get_queryset(self):
+        queryset = Guest.objects.all()
 
-        guest_name_param = request.query_params.get("guest_name", "")
-        phone_number_param = request.query_params.get("phone_number", "")
-        date_of_birth_param = request.query_params.get("date_of_birth", "")
-        guest_type_param = request.query_params.get("guest_type", "")
-        credit_card_number_param = request.query_params.get("credit_card_number", "")
-        email_param = request.query_params.get("email", "")
-        nationality_param = request.query_params.get("nationality", "")
-        job_param = request.query_params.get("job", "")
-        memo_param = request.query_params.get("memo", "")
+        # 쿼리 파라미터에서 필터링에 사용할 값들을 가져옵니다.
+        guest_name_param = self.request.query_params.get("guest_name", "")
+        phone_number_param = self.request.query_params.get("phone_number", "")
+        date_of_birth_param = self.request.query_params.get("date_of_birth", "")
+        guest_type_param = self.request.query_params.get("guest_type", "")
+        credit_card_number_param = self.request.query_params.get(
+            "credit_card_number", ""
+        )
+        email_param = self.request.query_params.get("email", "")
+        nationality_param = self.request.query_params.get("nationality", "")
+        job_param = self.request.query_params.get("job", "")
+        memo_param = self.request.query_params.get("memo", "")
 
+        # 쿼리셋에 필터를 적용합니다.
         if guest_name_param:
             queryset = queryset.filter(guest_name__icontains=guest_name_param)
 
@@ -60,6 +64,42 @@ class GuestListView(generics.ListCreateAPIView):
         if memo_param:
             queryset = queryset.filter(memo__icontains=memo_param)
 
-        serializer = GuestSerializer(queryset, many=True)
+        return queryset
 
+    def get(self, request, *args, **kwargs):
+        # 필터링에 사용할 파라미터들을 가져옵니다.
+        guest_name_param = self.request.query_params.get("guest_name", "")
+        phone_number_param = self.request.query_params.get("phone_number", "")
+        date_of_birth_param = self.request.query_params.get("date_of_birth", "")
+        guest_type_param = self.request.query_params.get("guest_type", "")
+        credit_card_number_param = self.request.query_params.get(
+            "credit_card_number", ""
+        )
+        email_param = self.request.query_params.get("email", "")
+        nationality_param = self.request.query_params.get("nationality", "")
+        job_param = self.request.query_params.get("job", "")
+        memo_param = self.request.query_params.get("memo", "")
+
+        # 모든 파라미터가 비어있는 경우
+        if not any(
+            [
+                guest_name_param,
+                phone_number_param,
+                date_of_birth_param,
+                guest_type_param,
+                credit_card_number_param,
+                email_param,
+                nationality_param,
+                job_param,
+                memo_param,
+            ]
+        ):
+            # 아무 데이터도 가져오지 않습니다.
+            queryset = Guest.objects.none()
+            serializer = GuestSerializer(queryset, many=True)
+            return render(request, self.template_name, {"guests": serializer.data})
+
+        # 파라미터가 있는 경우
+        queryset = self.get_queryset()
+        serializer = GuestSerializer(queryset, many=True)
         return render(request, self.template_name, {"guests": serializer.data})
