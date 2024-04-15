b5bfe6487882ce149b5a9dddcc3f955c251566d6
hanihoneykim <107341819+hanihoneykim@users.noreply.github.com>
2024-04-15 13:49:50 +0900

    add: Guest Model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Guest(models.Model):
+    GUEST_TYPE_CHOICES = [
+        ("individual", "개인"),
+        ("corporate", "법인"),
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, null=False, blank=False)
+    guest_name = models.CharField(max_length=40, null=True, blank=True)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    guest_type = models.CharField(
+        max_length=30, choices=GUEST_TYPE_CHOICES, blank=True, null=True
+    )
+    credit_card_number = models.CharField(max_length=50, null=True, blank=True)
+    email = models.CharField(max_length=50, null=True, blank=True)
+    nationality = models.CharField(max_length=20, null=True, blank=True)
+    job = models.CharField(max_length=50, null=True, blank=True)
+    memo = models.TextField(null=True, blank=True)
+
+    visit_count = models.IntegerField(null=True, blank=True)
---
+++ core/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Guest
+from the_myeongdong.models import MyeongdongReservation
+
+
+class GuestSerializer(serializers.ModelSerializer):
+    visit_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Guest
+        fields = "__all__"
+        read_only_fields = ("id", "visit_count")
+
+    def get_visit_count(self, obj):
+        # MyeongdongReservation 모델에서 'guest_name'과 'phone_number'가 일치하는 항목들의 개수를 가져옵니다.
+        visit_count = MyeongdongReservation.objects.filter(
+            guest_name=obj.guest_name, phone_number=obj.phone_number
+        ).count()
+        return visit_count
